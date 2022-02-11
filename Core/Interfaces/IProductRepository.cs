4cf1754b74ccd859bfc523582a849c957691fe4a
Alin Apostol <alin.cristian977@gmail.com>
2022-02-12 00:35:52 +0200

    added repo interface

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<Product>> GetProductsAsync();
+}

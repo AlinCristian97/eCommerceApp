a248fb07898f3b62c81b29db844fefc40fcc3d7d
Alin Apostol <alin.cristian977@gmail.com>
2022-02-16 19:01:09 +0200

    added concrete generic repository

---
+++ Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    public Task<T> GetByIdAsync(int idl)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}

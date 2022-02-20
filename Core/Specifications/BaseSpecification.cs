1e515ab1279866bbc35bdc1b182b9aa9d5ca32f2
Alin Apostol <alin.cristian977@gmail.com>
2022-02-20 02:48:32 +0200

    implemented base specification and added respective methods

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -10,7 +10,9 @@ public class BaseSpecification<T> : ISpecification<T>
 
     public Expression<Func<T, bool>> Criteria { get; }
     public List<Expression<Func<T, object>>> Includes { get; } = new();
-    
+    public Expression<Func<T, object>> OrderBy { get; private set; }
+    public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
     public BaseSpecification(Expression<Func<T, bool>> criteria)
     {
         Criteria = criteria;
@@ -19,5 +21,15 @@ public class BaseSpecification<T> : ISpecification<T>
     protected void AddInclude(Expression<Func<T, object>> includeExpression)
     {
         Includes.Add(includeExpression);
+    }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+    
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+    {
+        OrderByDescending = orderByDescendingExpression;
     }
 }

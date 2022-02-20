bc82e08be5175d038f24ba841803c85b370d2a7c
Alin Apostol <alin.cristian977@gmail.com>
2022-02-20 21:20:38 +0200

    update products count after filters to include the search filter

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -6,6 +6,8 @@ public class ProductWithFiltersForCountSpecification : BaseSpecification<Product
 {
     public ProductWithFiltersForCountSpecification(ProductSpecificationParams specificationParams)
         : base(x =>
+            (string.IsNullOrEmpty(specificationParams.Search) || x.Name.ToLower()
+                .Contains(specificationParams.Search)) &&
             (!specificationParams.BrandId.HasValue || x.ProductBrandId == specificationParams.BrandId) &&
             (!specificationParams.TypeId.HasValue || x.ProductTypeId == specificationParams.TypeId)
         )

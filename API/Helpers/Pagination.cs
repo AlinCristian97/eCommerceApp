d42782e4a3c4c3d6a62cfc27aa74290bfd46a5ba
Alin Apostol <alin.cristian977@gmail.com>
2022-02-20 21:01:50 +0200

    created constructor in pagination class

--- API/Helpers/Pagination.cs
+++ API/Helpers/Pagination.cs
@@ -6,4 +6,13 @@ public class Pagination<T> where T : class
     public int PageSize { get; set; }
     public int Count { get; set; }
     public IReadOnlyList<T> Data { get; set; }
+    
+    public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
+    {
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        Count = count;
+        Data = data;
+    }
+    
 }
